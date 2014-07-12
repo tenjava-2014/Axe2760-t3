1f782c5324d48a45b630c6b2ddb32bc291715bcf
Axe2760 <null>
2014-07-12 09:21:00 -0500

    decided on theme

--- src/main/java/com/tenjava/entries/Axe2760/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/Axe2760/t3/TenJava.java
@@ -2,4 +2,8 @@ package com.tenjava.entries.Axe2760.t3;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+    public void onEnable(){
+
+    }
+}
