1968a3532873bca59405bdf35879628fb818846c
Axe2760 <null>
2014-07-12 13:15:14 -0500

    world generatorz

--- src/main/java/com/tenjava/entries/Axe2760/t3/MoonGenerator.java
+++ src/main/java/com/tenjava/entries/Axe2760/t3/MoonGenerator.java
@@ -3,16 +3,32 @@ package com.tenjava.entries.Axe2760.t3;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.util.noise.PerlinNoiseGenerator;
 
 import java.util.Random;
 
 public class MoonGenerator extends ChunkGenerator{
+    private long seed;
+
+    public MoonGenerator(){
+        seed = new Random().nextLong();
+    }
+
+    public MoonGenerator(long seed){
+        this.seed = seed;
+    }
+
     @Override
-    public byte[][] generateBlockSections(World world, Random random, int x, int z, BiomeGrid biomes) {
+    public byte[][] generateBlockSections(World world, Random random, int chunkX, int chunkZ, BiomeGrid biomes) {
         byte[][] result = new byte[world.getMaxHeight() /16][];
-        for (x = 0; x < 16; x++){
-            for (z = 0; z < 16; z++){
+        PerlinNoiseGenerator generator = new PerlinNoiseGenerator(seed);
+        for (int x = 0; x < 16; x++){
+            for (int z = 0; z < 16; z++){
                 setBlock(result, x,0,z,Material.BEDROCK);
+                int topY = (int)Math.floor(generator.noise(chunkX * 16 + x,chunkZ * 16 + z) * 2)+50;
+                for (int y = 1; y <= topY; y++){
+                    setBlock(result,x,y,z,Material.STONE);
+                }
             }
         }
         return result;
